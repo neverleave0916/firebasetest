cf919a5b24c509a5a2f5b86cfd0a5a22c4991583
neverleave0916@gmail.com <neverleave0920>
2020-03-19 22:07:41 +0800

    Initial commit

---
+++ app/src/main/java/com/neverleave0916/firebasetest/FirstFragment.kt
@@ -0,0 +1,84 @@
+package com.neverleave0916.firebasetest
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        auth = FirebaseAuth.getInstance()
+        button.setOnClickListener { getUserProfile() }
+        button3.setOnClickListener { onClick() }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+    }
+
+    private fun onClick() {
+        Toast.makeText(activity,"登入中..", Toast.LENGTH_SHORT).show()
+        var email=et1.text.toString()
+        var password =et2.text.toString()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener((context as Activity?)!!) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(activity,"登入成功", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(activity,"登入失敗", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun getUserProfile() {
+        // [START get_user_profile]
+        Toast.makeText(activity,"使用者資料", Toast.LENGTH_SHORT).show()
+        val user = FirebaseAuth.getInstance().currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name1 = user.displayName
+            textView.text=name1.toString()
+            val email1 = user.email
+            textView2.text=email1.toString()
+            val photoUrl = user.photoUrl
+
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val uid = user.uid
+        }
+        // [END get_user_profile]
+    }
+}
---
+++ app/src/main/java/com/neverleave0916/firebasetest/SecondFragment.kt
@@ -0,0 +1,63 @@
+package com.neverleave0916.firebasetest
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_first.et1
+import kotlinx.android.synthetic.main.fragment_first.et2
+import kotlinx.android.synthetic.main.fragment_second.*
+
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Button>(R.id.button_second).setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+        button2.setOnClickListener { createAccount() }
+        auth = FirebaseAuth.getInstance()
+    }
+
+
+
+
+    private fun createAccount() {
+        Toast.makeText(activity,"註冊中..", Toast.LENGTH_SHORT).show()
+        // [START create_user_with_email]
+        var email=et1.text.toString()
+        var password =et2.text.toString()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener((context as Activity?)!!) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Toast.makeText(activity,"註冊成功", Toast.LENGTH_SHORT).show()
+                    val user = auth.currentUser
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(activity,"註冊失敗", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(activity, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        // [END create_user_with_email]
+    }
+}
